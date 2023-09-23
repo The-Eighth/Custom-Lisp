c797e50a697cdec0ce6207960ec4cd8bab717f6f
The Eighth <octavian.gomez@outlook.com>
2023-09-23 16:01:59 +0900

    Added support for both Win and linux cursor movement

--- Lisp/prompt.c
+++ Lisp/prompt.c
@@ -1,8 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*If OS is Windows then use the following functions*/
+#ifdef _WIN32
+
+#include <string.h>
+
+static char buffer[2048];
+
+/*Fake readline to replace linux functions*/
+char* readline(char* prompt){
+
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+
+	cpy[strlen(cpy)-1] = '\0';
+
+	return cpy;
+
+}
+
+/*Fake add_history. Unused*/
+
+void add_histpry(char* unused){}//Do nothing...
+
+
+/*Otherwise include the following headers as normal*/
+#else
+
 #include <editline/readline.h>
 #include <histedit.h>
+
+#endif
+
+
 
 
 int main(int argc, char** argv){
